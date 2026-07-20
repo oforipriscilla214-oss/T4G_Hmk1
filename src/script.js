767c1655818555149c1406042bcfba6307fe5855
oforipriscilla214-oss <oforipriscilla214@gmail.com>
2026-07-20 13:02:50 +0000

    Added user prompts for name, age, and email

---
+++ src/script.js
@@ -0,0 +1,7 @@
+//Taking input for the user and storing it in a variable called userName.
+let userName = prompt("Enter your name");
+let age = prompt("Enter your age:");
+let email = prompt("Enter your email:");
+
+    // Display the information
+    alert(`Hi ${name}, you are ${age} years old and we'll keep in touch via your email: ${email}`);
